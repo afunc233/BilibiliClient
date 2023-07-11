c20d8dbe5a8258eb67a32ac80b0f8fb41ee610c3
afunc <afunc@sina.com>
2023-07-11 21:42:54 +0800

    所有项目都可以编译通过

--- BilibiliClient.Android/MainActivity.cs
+++ BilibiliClient.Android/MainActivity.cs
@@ -4,7 +4,9 @@ using Avalonia.Android;
 
 namespace BilibiliClient.Android;
 
-[Activity(Label = "BilibiliClient.Android", Theme = "@style/MyTheme.NoActionBar", Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
-public class MainActivity : AvaloniaMainActivity
+[Activity(Label = "BilibiliClient.Android", Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon", LaunchMode = LaunchMode.SingleTop,
+    ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
+public class MainActivity : AvaloniaMainActivity<App>
 {
 }
---
+++ readme.md
@@ -0,0 +1,2 @@
+﻿# Tips  
+- browser-wasm 好像只能用 .net7 
